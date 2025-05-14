271dfe51b8f8b9711d73683edea6a878f9edb591
zzq <1030907690@qq.com>
2025-05-14 18:47:45 +0800

    feat:$fetch

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
+    <NuxtRouteAnnouncer/>
     112
-    <NuxtWelcome />
+    <NuxtWelcome/>
   </div>
 </template>
+<script lang="ts" setup>
+import {ref, onMounted} from 'vue'
+ let data = ref<string>("")
+onMounted(() => {
+  console.log("mount")
+  $fetch(`https://nuxt.com/docs/migration/component-options`).then(res => {
+    console.log("res ",res)
+    data.value = res as string
+  })
+  let query = {"prompt":"你好啊"}
+  $fetch(`http://127.0.0.1:20710/api/front/chatbot/chat`,{
+    method: 'POST',
+    body: JSON.stringify(query)
+  }).then(res => {
+    console.log("res ",res)
+  })
+})
+
+</script>
